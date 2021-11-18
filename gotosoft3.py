ada13bc6bcbc25ec21f3ba1f18c06f7cd159e573
felipe-vergaram <felip.vergara.m@gmail.com>
2021-11-18 18:32:16 -0300

    fix time delay

--- gotosoft3.py
+++ gotosoft3.py
@@ -15,11 +15,11 @@ robot_serial.open_serial()
 
 def gosoft(current_pose, goal, n_pasos):
     # Ajustar estos params en fn del tiempo
-    sleep_time = 0.5
+    sleep_time = 1
     goal[0] = min([max([goal[0], 0]), 90])
     goal[1] = min([max([goal[1], 40]), 150])
     goal[2] = min([max([goal[2], 60]), 120])
-
+    print("Goal:", goal)
 
     for i in range(n_pasos):
         for j in range(3):
@@ -36,7 +36,7 @@ try:
     q0val = int(input("q0: "))
     q1val = int(input("q1: "))
     q2val = int(input("q2: "))
-    current_pose = gosoft(current_pose, [q0val, q1val, q2val],10)
+    current_pose = gosoft(current_pose, [q0val, q1val, q2val], 5)
 
 
 except KeyboardInterrupt:
