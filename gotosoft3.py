510a66873857783e008c8d57c5c7274adeaa17d0
felipe-vergaram <felip.vergara.m@gmail.com>
2021-11-19 15:53:37 -0300

    add offline test

--- gotosoft3.py
+++ gotosoft3.py
@@ -1,37 +1,58 @@
 import sys
-
-sys.path.append('/home/pi/mlf/core')
-from serial_control import SerialControl
-from mk2robot import MK2Robot
-# from core.serial_control import SerialControl #for pc
-# from core.mk2robot import MK2Robot #for pc
 import time
 import numpy as np
 
-robot = MK2Robot(link_lengths=[55, 39, 135, 147, 66.3])
-robot_serial = SerialControl()
-# robot_serial = SerialControl("COM5") #for pc
-robot_serial.open_serial()
 
-def gosoft(current_pose, goal, n_pasos):
+
+on_raspi = True
+
+if on_raspi:
+    sys.path.append('/home/pi/mlf/core')
+    from serial_control import SerialControl
+    from mk2robot import MK2Robot
+    # from core.serial_control import SerialControl #for pc
+    # from core.mk2robot import MK2Robot #for pc
+
+    robot = MK2Robot(link_lengths=[55, 39, 135, 147, 66.3])
+    robot_serial = SerialControl()
+    # robot_serial = SerialControl("COM5") #for pc
+    robot_serial.open_serial()
+else:
+    import matplotlib.pyplot as plt
+
+def gosoft(current_pose, goal, n_pasos, on_raspi):
     # Ajustar estos params en fn del tiempo
-    sleep_time = 1
-    sleep_small_time = 0.7
+
     goal[0] = min([max([goal[0], 0]), 90])
     goal[1] = min([max([goal[1], 40]), 150])
     goal[2] = min([max([goal[2], 60]), 120])
     print("Goal:", goal)
 
-    for i in range(n_pasos):
-        for j in range(3):
-            target = int((current_pose[j]+goal[j])/2 +((current_pose[j]-goal[j])/2)*np.cos((i+1)*np.pi/n_pasos))
-            robot_serial.write_servo(j+1, target)
-            print("Motor:",j+1,"target",target)
-            time.sleep(sleep_small_time)
+    targets1 = [current_pose[0]]
+    targets2 = [current_pose[1]]
+    targets3 = [current_pose[2]]
 
-        #time.sleep(sleep_time)
-    print("final: ", goal)
-    return goal
+
+    for i in range(n_pasos):
+        target1 = int((current_pose[0]+goal[0])/2 +((current_pose[0]-goal[0])/2)*np.cos((i+1)*np.pi/n_pasos))
+        target2 = int((current_pose[1]+goal[1])/2 +((current_pose[1]-goal[1])/2)*np.cos((i+1)*np.pi/n_pasos))
+        target3 = int((current_pose[2]+goal[2])/2 +((current_pose[2]-goal[2])/2)*np.cos((i+1)*np.pi/n_pasos))
+        targets1.append(target1)
+        targets2.append(target2)
+        targets3.append(target3)
+        if on_raspi:
+            #sleep_time = 0.5
+            robot_serial.write_servo(1, target1)
+            #time.sleep(sleep_time)
+            robot_serial.write_servo(2, target2)
+            #time.sleep(sleep_time)
+            robot_serial.write_servo(3, target3)
+            #time.sleep(sleep_time)
+            time.sleep(1.5)
+        else:
+            time.sleep(0.05)
+
+    return goal, (targets1, targets2, targets3)
 
 
 current_pose = [45, 90, 90]
@@ -41,7 +62,14 @@ try:
         q1val = int(input("q1: "))
         q2val = int(input("q2: "))
         n_steps = int(input("n: "))
-        current_pose = gosoft(current_pose, [q0val, q1val, q2val], n_steps)
+        current_pose, targets = gosoft(current_pose, [q0val, q1val, q2val], n_steps, on_raspi)
+        for i in range(3):
+            plt.figure()
+            plt.plot(np.arange(len(targets[i])), targets[i])
+            plt.title("Q%i"%i)
+            plt.savefig("Q%i"%i)
+
+        break
 
 
 except KeyboardInterrupt:
