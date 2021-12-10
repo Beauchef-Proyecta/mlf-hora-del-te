40374f511cacf3127aa87314d85c429ae52d23b6
felipe-vergaram <felip.vergara.m@gmail.com>
2021-12-10 08:38:34 -0300

    deleted files from mlf-base repo

--- gotosoft3.py
+++ gotosoft3.py
@@ -6,7 +6,8 @@ import numpy as np
 # Agregar Gripper
 
 
-on_raspi = True
+on_raspi = False
+use_IK = True
 
 if on_raspi:
     sys.path.append('/home/pi/mlf/core')
@@ -22,13 +23,20 @@ if on_raspi:
 else:
     import matplotlib.pyplot as plt
 
-def gosoft(current_pose, goal, n_pasos, on_raspi):
+def gosoft(current_pose, goal, n_pasos, on_raspi, use_IK):
     # Ajustar estos params en fn del tiempo
+    if use_IK:
+        goal[0] = min([max([goal[0], 0]), 90])   # Definir límites
+        goal[1] = min([max([goal[1], 40]), 150]) # Definir límites
+        goal[2] = min([max([goal[2], 60]), 120]) # Definir límites
+        goal[3] = min([max([goal[3], 60]), 120]) # Definir límites
 
-    goal[0] = min([max([goal[0], 0]), 90])
-    goal[1] = min([max([goal[1], 40]), 150])
-    goal[2] = min([max([goal[2], 60]), 120])
-    goal[3] = min([max([goal[3], 60]), 120]) # Definir límites
+
+    else:
+        goal[0] = min([max([goal[0], 0]), 90])
+        goal[1] = min([max([goal[1], 40]), 150])
+        goal[2] = min([max([goal[2], 60]), 120])
+        goal[3] = min([max([goal[3], 60]), 120]) # Definir límites
     print("Goal:", goal)
 
     targets1 = [current_pose[0]]
@@ -42,6 +50,9 @@ def gosoft(current_pose, goal, n_pasos, on_raspi):
         target2 = int((current_pose[1]+goal[1])/2 +((current_pose[1]-goal[1])/2)*np.cos((i+1)*np.pi/n_pasos))
         target3 = int((current_pose[2]+goal[2])/2 +((current_pose[2]-goal[2])/2)*np.cos((i+1)*np.pi/n_pasos))
         target4 = int((current_pose[3]+goal[3])/2 +((current_pose[3]-goal[3])/2)*np.cos((i+1)*np.pi/n_pasos))
+        if use_IK:
+            (target1, target2, target3) = robot.inverse_kinematics(target1, target2, target3)
+
         targets1.append(target1)
         targets2.append(target2)
         targets3.append(target3)
@@ -65,12 +76,19 @@ def gosoft(current_pose, goal, n_pasos, on_raspi):
 current_pose = [45, 90, 90, 90]
 try:
     while True:
-        q0val = int(input("q0: "))
-        q1val = int(input("q1: "))
-        q2val = int(input("q2: "))
-        q3val = int(input("q3: "))
-        n_steps = int(input("n: "))
-        current_pose, targets = gosoft(current_pose, [q0val, q1val, q2val, q3val], n_steps, on_raspi)
+        if use_IK:
+            q0val = int(input("X: "))
+            q1val = int(input("Y: "))
+            q2val = int(input("Z: "))
+            q3val = int(input("Eff: "))
+            n_steps = int(input("n: "))
+        else:
+            q0val = int(input("q0: "))
+            q1val = int(input("q1: "))
+            q2val = int(input("q2: "))
+            q3val = int(input("Eff: "))
+            n_steps = int(input("n: "))
+        current_pose, targets = gosoft(current_pose, [q0val, q1val, q2val, q3val], n_steps, on_raspi, use_IK)
         if not on_raspi:
             for i in range(4):
                 plt.figure()
